b51ed5118ef393121082ffea0a77a5cb2869d1a9
Vivek Chetia <vivek.chetia@clinifyhealth.com>
2022-06-19 00:14:25 +0530

    day28: attended virtual interview ! ; linkedlist palindrom problem!

--- day28.js
+++ day28.js
@@ -25,20 +25,16 @@ function is_palindromic_linked_list(head) {
   // reverse the linkedlist from the middle
   const reverse_head = reverse_linked_list(slow);
   let temp_reverse_head = reverse_head;
-
   // compare the linkedlist value to check for palindrom
   let compareNode = head;
   while (compareNode !== null && temp_reverse_head !== null) {
-    if (compareNode.value !== temp_reverse_head.value) return false;
-
+    if (compareNode.value !== temp_reverse_head.value) break;
     compareNode = compareNode.next;
     temp_reverse_head = temp_reverse_head.next;
   }
-
   // revert the linkedlist
   reverse_linked_list(reverse_head);
-  if (compareNode === null && temp_reverse_head === null) return true;
-
+  if (compareNode === null || temp_reverse_head === null) return true;
   return false;
 }
 
@@ -58,6 +54,10 @@ function reverse_linked_list(head) {
   return prev;
 }
 
+// head = new LinkedList(1);
+// head.next = new LinkedList(2);
+// head.next.next = new LinkedList(2);
+// head.next.next.next = new LinkedList(1);
 head = new LinkedList(2);
 head.next = new LinkedList(4);
 head.next.next = new LinkedList(6);
