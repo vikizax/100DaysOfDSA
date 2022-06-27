df8f9996e44136b65f94f133e045c583749d6b4a
Vivek Chetia <vivek.chetia@clinifyhealth.com>
2022-06-28 00:11:30 +0530

    day37: more intervals!

---
+++ day37.js
@@ -0,0 +1,66 @@
+/** 
+Problem Statement#
+Given an array of intervals representing ‘N’ appointments, 
+find out if a person can attend all the appointments.
+
+Example 1:
+Appointments: [[1,4], [2,5], [7,9]]
+Output: false
+Explanation: Since [1,4] and [2,5] overlap, a person cannot attend both of these appointments.
+
+Example 2:
+Appointments: [[6,7], [2,4], [8,12]]
+Output: true
+Explanation: None of the appointments overlap, therefore a person can attend all of them.
+*/
+
+class Interval {
+  constructor(start, end) {
+    this.start = start;
+    this.end = end;
+  }
+
+  print_interval() {
+    process.stdout.write(`[${this.start}, ${this.end}]`);
+  }
+}
+
+/**
+ *
+ * @param {Interval[]} intervals
+ * @returns {boolean}
+ */
+function can_attend_all_appointments(intervals) {
+  intervals.sort((a, b) => a.start - b.start);
+
+  for (let i = 1; i < intervals.length; i++) {
+    if (intervals[i].start < intervals[i - 1].end) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(
+  `Can attend all appointments: ${can_attend_all_appointments([
+    new Interval(1, 4),
+    new Interval(2, 5),
+    new Interval(7, 9),
+  ])}`
+);
+
+console.log(
+  `Can attend all appointments: ${can_attend_all_appointments([
+    new Interval(6, 7),
+    new Interval(2, 4),
+    new Interval(8, 12),
+  ])}`
+);
+
+console.log(
+  `Can attend all appointments: ${can_attend_all_appointments([
+    new Interval(4, 5),
+    new Interval(2, 3),
+    new Interval(3, 6),
+  ])}`
+);
