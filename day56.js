53890dd98798fd353db410cf3bb269aae395a753
Vivek Chetia <vivek.chetia@clinifyhealth.com>
2022-07-20 23:38:48 +0530

    day56: more BFS traversal!

---
+++ day56.js
@@ -0,0 +1,44 @@
+/**
+Problem Statement#
+Given a binary tree, populate an array to represent the averages of all of its levels.
+ */
+
+class TreeNode {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function find_level_averages(root) {
+  const result = [];
+  const queue = [];
+
+  queue.push(root);
+
+  while (queue.length > 0) {
+    const levelSize = queue.length;
+    let totalSum = 0;
+    for (let i = 0; i < levelSize; i++) {
+      const currentNode = queue.shift();
+      totalSum += currentNode.value;
+
+      if (currentNode.left !== null) queue.push(currentNode.left);
+      if (currentNode.right !== null) queue.push(currentNode.right);
+    }
+    result.push(totalSum / levelSize);
+  }
+
+  return result;
+}
+
+const root = new TreeNode(12);
+root.left = new TreeNode(7);
+root.right = new TreeNode(1);
+root.left.left = new TreeNode(9);
+root.left.right = new TreeNode(2);
+root.right.left = new TreeNode(10);
+root.right.right = new TreeNode(5);
+
+console.log(`Level averages are: ${find_level_averages(root)}`);
